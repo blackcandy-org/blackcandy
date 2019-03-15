f93418141dc5b6c6f4f86c0faa52dcf0e4c19a74
aidewoode <aidewoode@gmail.com>
2019-03-15 22:31:22 +0800

    add dropdown component

---
+++ app/frontend/javascripts/controllers/dropdown_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from 'stimulus';
+
+export default class extends Controller {
+  static targets = ['list'];
+
+  show() {
+    this.listTarget.classList.remove('hidden');
+    document.addEventListener('click', () => { this.close(); }, { once: true, capture: true });
+  }
+
+  close() {
+    this.listTarget.classList.add('hidden');
+  }
+}
