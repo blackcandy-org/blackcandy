12a306ab74002d721273265aa6bdb01bf80567b6
aidewoode <aidewoode@gmail.com>
2020-01-14 16:56:29 +0800

    fix command not found issue for renew ssl script

--- docker/renew_ssl.sh
+++ docker/renew_ssl.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker-compose run --rm certbot renew --webroot -w /var/www/certbot --quiet
+/usr/local/bin/docker-compose run --rm certbot renew --webroot -w /var/www/certbot --quiet
 
-docker-compose kill -s HUP web
+/usr/local/bin/docker-compose kill -s HUP web
