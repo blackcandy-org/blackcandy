4dbfced5b99996b7b98b7b8f0ef03be175794369
aidewoode <aidewoode@gmail.com>
2020-07-16 15:34:51 +0800

    remove the environment variables required for installation

--- docker/production_start.sh
+++ docker/production_start.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+if [ -z ${var+x} ]; then
+  echo "Generating SECRET_KEY_BASE environment variable."
+  echo "Please attention, all old sessions will become invalid."
+  echo "You can set SECRET_KEY_BASE environment variable on docker service,"
+  echo "to avoid generate SECRET_KEY_BASE every time when service start up."
+  export SECRET_KEY_BASE=$(rails secret)
+fi
+
 # Prepare Database
 rails db:prepare
 
