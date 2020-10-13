7ba1b279650de51e920a61c09ac7d02254145e5f
aidewoode <aidewoode@gmail.com>
2020-10-13 11:08:03 +0800

    update development note on README

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -48,7 +48,7 @@ module ApplicationHelper
   end
 
   def format_duration(sec)
-    time = Time.at(sec)
+    time = Time.at(sec).utc
     sec > 1.hour ? time.strftime('%T') : time.strftime('%M:%S')
   end
 
--- docker/dev_entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-if [ -f tmp/pids/server.pid ]; then
-  rm tmp/pids/server.pid
-fi
-
-exec "$@"
