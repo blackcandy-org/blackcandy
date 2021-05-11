a62f1d7a98995c2499a8f4bb71add3f4dd28c306
aidewoode <aidewoode@gmail.com>
2021-05-11 15:34:08 +0800

    add missing test for controller

--- test/controllers/favorite_playlist/songs_controller_test.rb
+++ test/controllers/favorite_playlist/songs_controller_test.rb
@@ -8,4 +8,20 @@ class FavoritePlaylistSongsControllerTest < ActionDispatch::IntegrationTest
       assert_response :success
     end
   end
+
+  test 'should play favorite playlist' do
+    user = users(:visitor1)
+    playlist = user.favorite_playlist
+    playlist.song_ids = [1, 2]
+
+    assert_not_equal playlist.song_ids, user.current_playlist.song_ids
+
+    assert_login_access(
+      user: user,
+      method: :post,
+      url: play_favorite_playlist_songs_url
+    ) do
+      assert_equal playlist.song_ids, user.current_playlist.reload.song_ids
+    end
+  end
 end
