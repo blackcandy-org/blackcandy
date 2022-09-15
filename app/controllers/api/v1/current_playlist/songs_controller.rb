2a2075c053ab99e08f3d28b7ccb16e379be6402e
aidewoode <aidewoode@gmail.com>
2022-09-15 06:11:25 +0000

    Fix N+1 issue on API of  current playlist songs

--- app/controllers/api/v1/current_playlist/songs_controller.rb
+++ app/controllers/api/v1/current_playlist/songs_controller.rb
@@ -4,7 +4,12 @@ module Api
   module V1
     class CurrentPlaylist::SongsController < ApiController
       def show
+        favorite_playlist = Current.user.favorite_playlist
+
         @songs = Current.user.current_playlist.songs
+          .includes(:artist, :album)
+          .joins("Left JOIN playlists_songs T1 ON songs.id = T1.song_id AND T1.playlist_id = #{favorite_playlist.id}")
+          .select("songs.*, T1.playlist_id IS NOT NULL as is_favorited")
       end
     end
   end
