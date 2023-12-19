c042fc9b211249eac7c29fdc0ed45705d040736e
aidewoode <aidewoode@gmail.com>
2023-12-19 10:32:36 +0800

    Use puma hook to start and stop media listener service automatically

--- config/initializers/media_listener.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.config.after_initialize do
-  MediaListener.start if Setting.enable_media_listener? && !MediaListener.running?
-end
-
-at_exit do
-  MediaListener.stop if MediaListener.running?
-end
--- config/puma.rb
+++ config/puma.rb
@@ -23,13 +23,6 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
-# Specifies that the worker count should equal the number of processors in production.
-if ENV["RAILS_ENV"] == "production"
-  require "concurrent-ruby"
-  worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
-  workers worker_count if worker_count > 1
-end
-
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
@@ -39,6 +32,27 @@ end
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+on_booted do
+  MediaListener.start if Setting.enable_media_listener? && !MediaListener.running?
+end
+
+if ENV["RAILS_ENV"] == "production"
+  # Specifies that the worker count should equal the number of processors in production.
+  require "concurrent-ruby"
+  worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
+  workers worker_count if worker_count > 1
+
+  on_worker_shutdown do
+    MediaListener.stop if MediaListener.running?
+  end
+end
+
+if ENV["RAILS_ENV"] == "development"
+  on_thread_exit do
+    MediaListener.stop if MediaListener.running?
+  end
+end
 
 if BlackCandy::Config.embedded_sidekiq?
   # Preloading the application is necessary to ensure
