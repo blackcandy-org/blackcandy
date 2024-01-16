f41d96be24882cadc6f5bc6151cb22b8859eca23
aidewoode <aidewoode@gmail.com>
2024-01-16 21:37:33 +0800

    Try to fix CI errors

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -56,6 +56,4 @@ Rails.application.configure do
   config.action_controller.raise_on_missing_callback_actions = true
 
   config.action_controller.action_on_unpermitted_parameters = :raise
-
-  config.active_job.queue_adapter = :test
 end
--- test/system/setting_test.rb
+++ test/system/setting_test.rb
@@ -24,7 +24,7 @@ class SettingSystemTest < ApplicationSystemTestCase
     fill_in("setting_media_path", with: Rails.root.join("test/fixtures/files").to_s)
     click_on("Sync")
 
-    assert_equal "Syncing...", find(:test_id, "media_sync_button").text
+    assert_text("Media sync completed")
   end
 
   test "update discogs token" do
