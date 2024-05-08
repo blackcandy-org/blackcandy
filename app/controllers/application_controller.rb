ce26e22269818e38f4cfc692a8e24c4081b61d6e
aidewoode <aidewoode@gmail.com>
2024-05-08 15:12:20 +0800

    Transcode ALAC format for non-safari browsers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -44,7 +44,9 @@ class ApplicationController < ActionController::Base
   def need_transcode?(song)
     song_format = song.format
 
-    return true unless song_format.in?(Stream::WEB_SUPPORTED_FORMATS)
+    return true if !browser.safari? && !song_format.in?(Stream::WEB_SUPPORTED_FORMATS)
+    # Non-Safari browsers don't support ALAC format. So we need to transcode it.
+    return true if !browser.safari? && song_format == "m4a" && song.lossless?
     return true if browser.safari? && !song_format.in?(Stream::SAFARI_SUPPORTED_FORMATS)
     return true if ios_app? && !song_format.in?(Stream::IOS_SUPPORTED_FORMATS)
     return true if android_app? && !song_format.in?(Stream::ANDROID_SUPPORTED_FORMATS)
