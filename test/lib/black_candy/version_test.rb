90120d329ab2e23b4d11f12297a5676d7e666fe0
Ed Chao <aidewoode@gmail.com>
2026-07-23 20:39:44 +0900

    test: fix flaky version test parallel filesystem race

--- test/lib/black_candy/version_test.rb
+++ test/lib/black_candy/version_test.rb
@@ -5,21 +5,20 @@ require "test_helper"
 class BlackCandy::VersionTest < ActiveSupport::TestCase
   test "should get version string and link" do
     with_env("COMMIT_HASH" => "abcdef1234567890") do
-      assert_equal "abcdef1234567890", BlackCandy::Version.commit_hash
-      assert_match(/^v\d+\.\d+\.\d+$/, BlackCandy::Version.to_s)
-      assert_match(/github\.com\/blackcandy-org\/blackcandy\/releases\/tag\/v\.*/, BlackCandy::Version.link)
+      BlackCandy::Version.stub(:edge_release?, false) do
+        assert_equal "abcdef1234567890", BlackCandy::Version.commit_hash
+        assert_match(/^v\d+\.\d+\.\d+$/, BlackCandy::Version.to_s)
+        assert_match(/github\.com\/blackcandy-org\/blackcandy\/releases\/tag\/v\.*/, BlackCandy::Version.link)
+      end
     end
   end
 
   test "should get edge version string and link when it is edge release" do
-    file_path = Rails.root.join(".is-edge-release.txt")
-    FileUtils.touch(file_path)
-
     with_env("COMMIT_HASH" => "abcdef1234567890") do
-      assert_equal "version: edge(abcdef1)", BlackCandy::Version.to_s
-      assert_match(/github\.com\/blackcandy-org\/blackcandy\/commit\/abcdef1234567890/, BlackCandy::Version.link)
+      BlackCandy::Version.stub(:edge_release?, true) do
+        assert_equal "version: edge(abcdef1)", BlackCandy::Version.to_s
+        assert_match(/github\.com\/blackcandy-org\/blackcandy\/commit\/abcdef1234567890/, BlackCandy::Version.link)
+      end
     end
-  ensure
-    FileUtils.remove_file(file_path)
   end
 end
