151446cd26252ba2edf1ddf2f197885d032385d7
Ed Chao <aidewoode@gmail.com>
2026-07-30 20:23:11 +0900

    fix: ci

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class SongsController < ApplicationController
-  render_in_dialog :edit
+  render_in_dialog only: :edit
 
   before_action :require_admin, only: [ :edit, :update ]
   before_action :find_song, except: [ :index ]
